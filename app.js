10cf21e8d7b566a0803952200fe13b02923edb70
Norman <norman@Normans-MacBook.local>
2015-06-25 19:17:05 -0700

    added portfolio and javascript html

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("nfdnfgjk");
+
+   <div class="form-group">
+        <div class="col-sm-10 col-sm-offset-2">
+            <input id="submit" name="submit" type="submit" value="Send">
+            <input type="submit" onclick="return confirm('Are you sure?')" /
+      var button = document.querySelector("#email a");
+            class="btn btn-primary">
+
+        </div>
